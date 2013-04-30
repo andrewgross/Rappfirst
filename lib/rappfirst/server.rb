3b96b5bce27494fbfa4d9f46c7f2732910efecd0
Andrew Gross <andrew@yipit.com>
2013-04-30 19:05:44 -0400

    Authentication with tests

--- lib/rappfirst/server.rb
+++ lib/rappfirst/server.rb
@@ -5,5 +5,8 @@ module Rappfirst
 
     base_uri 'https://wwws.appfirst.com/api/v3'
 
+    config = YAML::load( File.open( 'config.yml') )
+
+    basic_auth config['username'], config['password']
   end
 end
--- spec/lib/rappfirst/server_spec.rb
+++ spec/lib/rappfirst/server_spec.rb
@@ -12,6 +12,28 @@ describe Rappfirst::Server do
       Rappfirst::Server.base_uri.must_equal 'https://wwws.appfirst.com/api/v3'
     end
  
+    it "must have API Credentials" do
+      Rappfirst::Server.
+      instance_variable_get("@default_options").
+      keys.must_include(:basic_auth)
+    end
+
   end
  
+  describe "GET profile" do
+ 
+    before do
+      VCR.insert_cassette 'server', :record => :new_episodes
+    end
+   
+    after do
+      VCR.eject_cassette
+    end
+   
+    it "records the fixture" do
+      Rappfirst::Server.get('/servers/')
+    end
+   
+  end
+
 end
