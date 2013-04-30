df223549028b861dde11150f4a342f5973f3fba1
Andrew Gross <andrew@yipit.com>
2013-04-30 17:36:53 -0400

    Added base uri and HTTParty

--- lib/rappfirst/server.rb
+++ lib/rappfirst/server.rb
@@ -1,4 +1,9 @@
 module Rappfirst
   class Server
+
+    include HTTParty
+
+    base_uri 'https://wwws.appfirst.com/api/v3'
+
   end
 end
--- spec/lib/rappfirst/server_spec.rb
+++ spec/lib/rappfirst/server_spec.rb
@@ -2,8 +2,16 @@ require_relative '../../spec_helper'
 
 describe Rappfirst::Server do
  
-  it "must work" do
-    "Yay!".must_be_instance_of String
+  describe "default attributes" do
+ 
+    it "must include httparty methods" do
+      Rappfirst::Server.must_include HTTParty
+    end
+ 
+    it "must have the base url set to the Appfirst API endpoint" do
+      Rappfirst::Server.base_uri.must_equal 'https://wwws.appfirst.com/api/v3'
+    end
+ 
   end
  
 end
